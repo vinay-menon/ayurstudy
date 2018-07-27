35c642b68aa6109352b30c8463160ba1faf7508b
krishnan-akhil315 <krishnanakhil315@gmail.com>
2018-07-27 17:17:41 +0530

    meow

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// https://codepen.io/BoyWithSilverWings/pen/MJgQqR?editors=0010
+
+
+$.fn.isInViewport = function() {
+  var elementTop = $(this).offset().top;
+  var elementBottom = elementTop + $(this).outerHeight();
+
+  var viewportTop = $(window).scrollTop();
+  var viewportBottom = viewportTop + $(window).height();
+
+  return elementBottom > viewportTop && elementTop < viewportBottom;
+};
+
+$(window).on('resize scroll', function() {
+  $('.info-box').each(function() {
+    if ($(this).isInViewport()) {
+      $(this).addClass('slide-in');
+    } else {
+      $(this).removeClass('slide-in');
+    }
+  });
+});
